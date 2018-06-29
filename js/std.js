f04915377991102dd118536bc4f3175c5e3f3814
beatacable <31115196+beatacable@users.noreply.github.com>
2018-06-28 19:28:22 -0500

    Add files via upload

--- js/std.js
+++ js/std.js
@@ -1,41 +1,27 @@
-// standardDeviation:
+function sum(x)
+{
+	var sum = 0;
+	for(i = 0; i < x.length; i++)
+	{
+		sum = sum + x[i];
+	}
+	return sum;
+};
 
-function variance(arr)
+function mean(x)
 {
-var len = 0;
-var sum=0;
-for(var i=0;i<arr.length;i++)
-{
-if (arr[i] == ""){}
-else if (!isNum(arr[i]))
-{
-alert(arr[i] + " is not number, Variance Calculation failed!");
-return 0;
-}
-else
-{
-len = len + 1;
-sum = sum + parseFloat(arr[i]);
-}
-}
-var v = 0;
-if (len > 1)
-{
-var mean = sum / len;
-for(var i=0;i<arr.length;i++)
-{
-if (arr[i] == ""){}
-else
-{
-v = v + (arr[i] - mean) * (arr[i] - mean);
-}
-}
-return v / len;
-}
-else
-{
-return 0;
-}
-}
+return sum(x) / x.length;
+};
 
-var sd = Math.sqrt(variance(arr));
+function variance(x) {
+		var mean = mean(x);
+		return mean(array.map(sum(sum)) {
+			return Math.pow(sum - mean, 2);
+		}));
+	},
+
+
+function standardDeviation(x)
+{
+		return Math.sqrt(variance(x));
+};
---
+++ js/sum.js
@@ -0,0 +1,9 @@
+function sum(x)
+{
+	var sum = 0;
+	for(i = 0; i < x.length; i++)
+	{
+		sum = sum + x[i];
+	}
+	return sum;
+};
