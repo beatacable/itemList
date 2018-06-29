96324761025a39b7dffb8dd32431335ae40f4ba3
beatacable <31115196+beatacable@users.noreply.github.com>
2018-06-29 11:50:18 -0500

    Add files via upload

--- js/calc.js
+++ js/calc.js
@@ -1,5 +1,4 @@
 
-
 // calculation for: addition, subtraction and division
 function calc() {
   var n1 = parseFloat(document.getElementById('n1').value);
@@ -21,25 +20,3 @@ function calc() {
     document.getElementById('result').value = n1 / n2;
   }
 }
-=======
-function calc() {
-  var n1 = parseFloat(document.getElementById('n1').value);
-  var n2 = parseFloat(document.getElementById('n2').value);
-  var oper = document.getElementById('operators').value;
-
-  if(oper === '+')
-  {
-    document.getElementById('result').value = n1 + n2;
-  }
-
-  if(oper === '-')
-  {
-    document.getElementById('result').value = n1 - n2;
-  }
-
-  if(oper === '/')
-  {
-    document.getElementById('result').value = n1 / n2;
-  }
-}
-
